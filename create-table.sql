1ba25a7b60354743b8bfc9c0a1b59efffb4e5e7e
Istia Budi <istiabudi@gmail.com>
2019-04-28 14:58:22 +0800

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -2,6 +2,6 @@ create table [dbo].[absensi](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
     nama VARCHAR(50),
     nim VARCHAR(50),
-    npk VARCHAR(50),
+    nik VARCHAR(50),
     date DATE
 );
