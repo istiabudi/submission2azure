cc39cd99c3752df39e770b8b6bccfeb90c0ab04b
Istia Budi <istiabudi@gmail.com>
2019-04-29 16:56:10 +0800

    Update analyze.php

--- analyze.php
+++ analyze.php
@@ -9,127 +9,19 @@ use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=azurebejo;AccountKey=lUAaDVuJkw4mR793S4YJxLEsu+izo9kI+t/ZCMWTfN1ArphX2UJAgHxsMp31Icpqu84M5VUpq312abNQHK6l4g==;";
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-// $fileToUpload = "HelloWorld.txt";
-// $fileToUpload = "D:\home\site\wwwroot\halaman\uploads\Jellyfish.jpg";
-$files = glob("D:\home\site\wwwroot\halaman\*.{jpg,gif,png}", GLOB_BRACE);
-	usort( $files, function( $a, $b ) { return filemtime($b) - filemtime($a); } );
-	$fileToUpload = $files[0];
-	$filePath = $files[0];
-	$fileToUpload = basename($filePath);
-	$filePrefix = basename($filepath, ".jpg");
-	$filePrefix = basename($filepath, ".jpeg");
-	$filePrefix = basename($filepath, ".png");
-	$filePrefix = basename($filepath, ".gif");
-if (!isset($_GET["Cleanup"])) {
-    // Create container options object.
-    $createContainerOptions = new CreateContainerOptions();
-    // Set public access policy. Possible values are
-    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-    // CONTAINER_AND_BLOBS:
-    // Specifies full public read access for container and blob data.
-    // proxys can enumerate blobs within the container via anonymous
-    // request, but cannot enumerate containers within the storage account.
-    //
-    // BLOBS_ONLY:
-    // Specifies public read access for blobs. Blob data within this
-    // container can be read via anonymous request, but container data is not
-    // available. proxys cannot enumerate blobs within the container via
-    // anonymous request.
-    // If this value is not specified in the request, container data is
-    // private to the account owner.
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
-      $containerName = "blockblobs".generateRandomString();
-    try {
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
-        // Getting local file so that we can upload it to Azure
-        $myfile = fopen($filePath, "r") or die("Unable to open file!");
-        fclose($myfile);
-        
-        # Upload file as a block blob
-        echo "Uploading BlockBlob: ".PHP_EOL;
-        echo $fileToUpload;
-        echo "<br />";
-        
-        $content = fopen($filePath, "r");
-        //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-        // List blobs.
-        $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix($filePrefix);
-        echo "These are the blobs present in the container: ";
-        do{
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob)
-            {
-		$imgUrl = $blob->getUrl();
-                echo $blob->getName().": ".$blob->getUrl()."<br />";
-		echo "<a href='$imgUrl'>Click Here To Download</a><br /><br />";
-            }
-        
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while($result->getContinuationToken());
-        echo "<br />";
-        // Get blob.
-        echo "if there are characters that you cannot read,<br />";
-	echo "that means this file is not a text file <br />";
-	echo "This is the content of the blob uploaded: <br /><br />";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        fpassthru($blob->getContentStream());
-        echo "<br />";
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-    catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-} 
-else 
-{
-    try{
-        // Delete container.
-        echo "Deleting Container".PHP_EOL;
-        echo $_GET["containerName"].PHP_EOL;
-        echo "<br />";
-        $blobClient->deleteContainer($_GET["containerName"]);
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-}
-//$containerName = "blobistia";
+$containerName = "blobistia";
 // Create blob client.
-//$blobClient = BlobRestProxy::createBlobService($connectionString);
-//if (isset($_POST['submit'])) {
-//	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
-//	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-//	// echo fread($content, filesize($fileToUpload));
-//	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-//	header("Location: analyze.php");
-//}
-//$listBlobsOptions = new ListBlobsOptions();
-//$listBlobsOptions->setPrefix("");
-//$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+if (isset($_POST['submit'])) {
+	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
+	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+	// echo fread($content, filesize($fileToUpload));
+	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+	header("Location: analyze.php");
+}
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 ?>
 
 <!DOCTYPE html>
